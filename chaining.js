082c2f999a94901662e7928910fe89b12f0d1f1d
RabeyaEty01 <etyrabeya01@gmail.com>
2021-08-27 13:34:52 +0600

    chaining

--- chaining.js
+++ chaining.js
@@ -2,7 +2,7 @@
 
 const myObject = { x: 2, y: 50, z: 600, a: 25, b: 68 };
 const { x, y, b } = myObject;
-console.log(x, b);
+//console.log('myObject.p', myObject?.p?.q); //optional chaining
 
 //destructuring array 1
 const [p, q] = [45, 37, 91, 86];
@@ -14,3 +14,26 @@ console.log(best, faltu);
 
 const { sky, color } = { sky: 'blue', soil: 'mati', color: 'red' }
 console.log(color);
+
+
+
+//chaining
+
+
+const company = {
+    name: 'GP',
+    ceo: { cid: 1, name: 'ajmol', food: 'fuchka' },
+    web: {
+        work: 'website development',
+        employee: 22,
+        framework: 'react',
+        tech: {
+            first: 'html',
+            second: 'css',
+            third: 'js'
+        }
+    }
+};
+//console.log(company.web.tech.third);
+console.log(company?.backend?.tech.third);
+//kono karone kono ekta value jodi undefined hoye jay oita rodh korar jonno ? eta use korbo . etake optional chaining.
